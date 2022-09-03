823e8b9e511cabe60066f91d5c85445fb0109145
Jocinei da Rosa <jocinei.prog@gmail.com>
2022-09-02 21:15:08 -0300

    modificado visual dashboard

--- resources/views/app/layouts/dashboard.blade.php
+++ resources/views/app/layouts/dashboard.blade.php
@@ -1,29 +1,47 @@
 @extends('app.layouts.app')
 @section('titulo', 'Dashboard')
 <main class="content">
-    <div class="card">
-        <div class="card-header-template">
-            <div>
-                <i class="icofont-cubes mr-2"></i>
-                Estoque de Produtos
-            </div>
-            <div>
-            </div>
-        </div>
-        <div class="card-body">
 
-            @foreach ($estoque_produtos as $estoque)
-            <div class="painel-estoque-dashboard">
-                <div class="field-dash">
-                    {{$estoque->nome}}
-                </div>
-                <div class="field-dash">
-                    {{$estoque->estoque_atual}}
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                <i class="icofont-leaflet mr-2"></i>Estoque de Produtos</a>
+        </div>
+
+        <!-- Content Row -->
+        <div class="row">
+            @foreach ($estoque_produtos as $estoque )
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    {{$estoque->nome}}</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$estoque->estoque_atual}}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="icofont-navigation-menu"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+                
             @endforeach
+
+            
+
         </div>
     </div>
+
+
+
+
 
 
 </main>
--- resources/views/app/layouts/header.blade.php
+++ resources/views/app/layouts/header.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{asset('css/sb/sb-admin.css')}}">
     <link rel="stylesheet" href="{{ asset('css/comum.css') }}">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{ asset('css/icofont.min.css')}}">
