3bc72ad9e99d401fcce28be57d2189003538c846
Jocinei da Rosa <jocinei.prog@gmail.com>
2022-09-25 17:45:11 -0300

    colocado autonomia e mais dados no estoque do dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\OrdemProducao;
 use App\Models\Produto;
+use App\Models\RecursosProducao;
+use GuzzleHttp\Handler\Proxy;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,12 +30,29 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $estoque_produtos = Produto::whereIn('id', [1, 2, 13])->get();
+        $last_data_production=OrdemProducao::max('data');
+        $last_production=OrdemProducao::where('data',$last_data_production)->first();
+        $recursos= RecursosProducao::where('ordem_producao_id', $last_production->id)->where('quantidade', '>', 1)->get();
+        foreach($recursos as $recurso){
+            $estoque= Produto::find($recurso->produto_id);
+            $recurso->percent_estoque = round($estoque->estoque_atual / $estoque->estoque_maximo * 100 , 0);
+            $recurso->estoque_util= $estoque->estoque_atual - $estoque->lastro;
+            $teor= $recurso->quantidade / $last_production->quantidade_producao;
+            $recurso->estoque_maximo= $estoque->estoque_maximo;
+            $recurso->teor=$teor;
+            $recurso->estoque_atual= $estoque->estoque_atual;
+            $recurso->nome_prod= $recurso->produto->nome;
+            $recurso->autonomia= round($recurso->estoque_util / $teor);
+        }
+
+     /*    $estoque_produtos = Produto::whereIn('id', [1, 2, 13])->get();
         foreach ($estoque_produtos as $estoque) {
             $estoque->percent_estoque = $estoque->estoque_atual / $estoque->estoque_maximo * 100;
             $estoque->percent_estoque= round($estoque->percent_estoque,0);
-        }
-        return view('app.layouts.dashboard', ['estoque_produtos' => $estoque_produtos]);
+            $estoque->estoque_util= $estoque->estoque_atual - $estoque->lastro;
+            $estoque->autonomia= round($estoque->estoque_util / 0.053 , 0);
+        } */
+        return view('app.layouts.dashboard', ['recursos' => $recursos]);
         //return ('chegameos aqui');
     }
 }
--- resources/views/app/layouts/dashboard.blade.php
+++ resources/views/app/layouts/dashboard.blade.php
@@ -1,54 +1,62 @@
 @extends('app.layouts.app')
 @section('content')
-<div class="card">
-    <div class="card-header-template">
-        <div>
-            ESTOQUES PRINCIPAIS
+    <div class="card">
+        <div class="card-header-template">
+            <div>
+                ESTOQUES PRINCIPAIS
+            </div>
+            <div>
+                <a class="btn btn-primary btn-sm mr-2" href="{{ route('ordem-producao.create') }}"></i>PRODUTOS
+                </a>
+            </div>
         </div>
-        <div>
-            <a class="btn btn-primary btn-sm mr-2" href="{{ route('ordem-producao.create') }}"></i>PRODUTOS
-            </a>
-        </div>
-    </div>
 
-    <!-- Content Row -->
-    <div class="row">
-        @foreach ($estoque_produtos as $estoque)
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-primary shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="h5 font-weight-bold text-primary text-uppercase mb-1">
-                                    {{ $estoque->nome }}</div>
-                                <div class="h5 mb-2 font-weight-bold  text-success">
-                                    ESTOQUE: {{str_replace(',','.',number_format($estoque->estoque_atual, 0))}}
-                                </div>
-                                <div class="h5 mb-3 ">
-                                    MÁX: {{$estoque->estoque_maximo}}
-                                </div>
+        <!-- Content Row -->
+        <div class="row">
+            @foreach ($recursos as $recurso)
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="font-weight-bold text-primary text-uppercase ">
+                                        {{ $recurso->nome_prod}}
+                                    </div>
+                                    <hr>
+                                    <div class="mb-2 font-weight-bold  text-success">
+                                        ESTOQUE: {{ str_replace(',', '.', number_format($recurso->estoque_atual, 0)) }}
+                                    </div>
+                                    <div class="mb-2 font-weight-bold  text-success">
+                                        PARA USO: {{$recurso->estoque_util}}
+                                    </div>
+                                    <div class="mb-2 font-weight-bold  text-success">
+                                        AUTONOMIA: {{str_replace(',','.',number_format($recurso->autonomia, 0))}}
+                                    </div>
+                                    <div class="mb-3 ">
+                                        ESTQ MÁX: {{str_replace(',','.',number_format($recurso->estoque_maximo,0))}}
+                                    </div>
 
-                                <div class="col">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                                        {{ $estoque->percent_estoque }}%</div>
-                                    <div class="progress progress-sm mr-2">
+                                    <div class="col">
+                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                                            {{ $recurso->percent_estoque }}%</div>
+                                        <div class="progress progress-sm mr-2">
 
-                                        <div class="progress-bar {{ $estoque->percent_estoque > 30 ? 'bg-primary' : 'bg-danger' }} 
-                                            {{ $estoque->percent_estoque > 60 ? 'bg-success' : '' }}"
-                                            role="progressbar" style="width: {{ $estoque->percent_estoque }}%"
-                                            aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                            <div class="progress-bar {{ $recurso->percent_estoque > 30 ? 'bg-primary' : 'bg-danger' }} 
+                                            {{ $recurso->percent_estoque > 60 ? 'bg-success' : '' }}"
+                                                role="progressbar" style="width: {{ $recurso->percent_estoque }}%"
+                                                aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="icofont-navigation-menu"></i>
+                                <div class="col-auto">
+                                    <i class="icofont-navigation-menu"></i>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
 
+        </div>
     </div>
-</div>
 @endsection
