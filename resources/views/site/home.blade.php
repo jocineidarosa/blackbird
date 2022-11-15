afc01ce2865f28e40e48af8f8f098c159e4f0709
Jocinei da Rosa <jocinei.prog@gmail.com>
2022-11-15 20:55:01 -0300

    Iniciado site publico

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,7 +1,7 @@
 @extends('site.main')
 @section('content')
-<div class="container-fluid">
-  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+<div class="container-fluid" style="margin: 0;">
+  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel" style="margin: 0;">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
--- resources/views/site/topbar.blade.php
+++ resources/views/site/topbar.blade.php
@@ -1,46 +1,37 @@
      <!-- top navigation bar -->
      <header>
-         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+         <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
              <div class="container-fluid px-5">
-                 <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar"
-                     aria-controls="offcanvasExample">
-                     <span class="navbar-toggler-icon" data-bs-target="#sidebar"></span>
+                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                     aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                     <span class="navbar-toggler-icon"></span>
                  </button>
                  <a class="navbar-brand me-auto ms-lg-0 ms-3" href="{{ route('site.home') }}">
                      <img src="{{ asset('img/site/logo.png') }}" style="max-width:165px" alt="Bresola Terraplanagem" />
                  </a>
-                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#topNavBar"
-                     aria-controls="topNavBar" aria-expanded="false" aria-label="Toggle navigation">
-                     <span class="navbar-toggler-icon"></span>
-                 </button>
-                 <ul class="navbar-nav">
-                     <li class="nav-item dropdown">
-                         <a class="nav-link ms-2" href="{{ route('site.home') }}">
-                             Início
-                         </a>
-                     </li>
+                 <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarCollapse">
+                     <ul class="navbar-nav">
+                         <li class="nav-item mx-2">
+                             <a class="nav-link" href="{{route('site.home')}}">Início</a>
+                         </li>
 
-
-                     <li class="nav-item dropdown">
-                         <a class="nav-link ms-2" href="#">
-                             Obras
-                         </a>
-                     </li>
-
-                     <li class="nav-item dropdown">
-                         <a class="nav-link ms-2" href="">
-                             Trabalhe Conosco
-                         </a>
-                     </li>
-
-                     <li class="nav-item dropdown">
-                         <a class="nav-link ms-2" href="{{ route('app.home') }}" target="_blank">
-                             Área restrita
-                         </a>
-                     </li>
-
-                 </ul>
-             </div>
+                         <li class="nav-item mx-2">
+                            <a class="nav-link" href="#">Empresa</a>
+                        </li>
+                         <li class="nav-item mx-2">
+                            <a class="nav-link" href="#">Contato</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="#">Trabalhe Conosco</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" target="_blank" href="https://webmail.bresola.com.br/?_task=logout&_token=i0ZcFlJzn1O4wdBYEWwmC5oEq40hEDOx">Webmail</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="{{route('app.home')}}">Área restrita</a>
+                        </li>
+                     </ul>
+                 </div>
              </div>
          </nav>
      </header>
