de975db8d1bf205a64712bf74b5bc45d01046acb
Jocinei da Rosa <jocinei.prog@gmail.com>
2023-02-18 13:19:31 -0300

    modificado MedidasTanque3Seeder

---
+++ database/seeders/MedidasTanqueDiesel.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MedidasTanqueDiesel extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	0	', 'quantidade' => '	0	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	1	', 'quantidade' => '	9	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	2	', 'quantidade' => '	18	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	3	', 'quantidade' => '	27	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	4	', 'quantidade' => '	37	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	5	', 'quantidade' => '	46	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	6	', 'quantidade' => '	55	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	7	', 'quantidade' => '	64	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	8	', 'quantidade' => '	73	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	9	', 'quantidade' => '	82	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	10	', 'quantidade' => '	92	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	11	', 'quantidade' => '	101	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	12	', 'quantidade' => '	110	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	13	', 'quantidade' => '	119	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	14	', 'quantidade' => '	128	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	15	', 'quantidade' => '	137	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	16	', 'quantidade' => '	147	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	17	', 'quantidade' => '	156	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	18	', 'quantidade' => '	165	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	19	', 'quantidade' => '	174	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	20	', 'quantidade' => '	183	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	21	', 'quantidade' => '	192	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	22	', 'quantidade' => '	202	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	23	', 'quantidade' => '	211	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	24	', 'quantidade' => '	220	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	25	', 'quantidade' => '	229	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	26	', 'quantidade' => '	238	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	27	', 'quantidade' => '	247	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	28	', 'quantidade' => '	256	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	29	', 'quantidade' => '	266	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	30	', 'quantidade' => '	275	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	31	', 'quantidade' => '	284	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	32	', 'quantidade' => '	293	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	33	', 'quantidade' => '	302	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	34	', 'quantidade' => '	311	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	35	', 'quantidade' => '	321	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	36	', 'quantidade' => '	330	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	37	', 'quantidade' => '	339	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	38	', 'quantidade' => '	348	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	39	', 'quantidade' => '	357	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	40	', 'quantidade' => '	366	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	41	', 'quantidade' => '	376	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	42	', 'quantidade' => '	385	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	43	', 'quantidade' => '	394	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	44	', 'quantidade' => '	403	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	45	', 'quantidade' => '	412	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	46	', 'quantidade' => '	421	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	47	', 'quantidade' => '	431	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	48	', 'quantidade' => '	440	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	49	', 'quantidade' => '	449	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	50	', 'quantidade' => '	458	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	51	', 'quantidade' => '	467	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	52	', 'quantidade' => '	476	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	53	', 'quantidade' => '	485	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	54	', 'quantidade' => '	495	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	55	', 'quantidade' => '	504	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	56	', 'quantidade' => '	513	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	57	', 'quantidade' => '	522	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	58	', 'quantidade' => '	531	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	59	', 'quantidade' => '	540	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	60	', 'quantidade' => '	550	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	61	', 'quantidade' => '	559	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	62	', 'quantidade' => '	568	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	63	', 'quantidade' => '	577	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	64	', 'quantidade' => '	586	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	65	', 'quantidade' => '	595	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	66	', 'quantidade' => '	605	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	67	', 'quantidade' => '	614	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	68	', 'quantidade' => '	623	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	69	', 'quantidade' => '	632	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	70	', 'quantidade' => '	641	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	71	', 'quantidade' => '	650	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	72	', 'quantidade' => '	660	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	73	', 'quantidade' => '	669	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	74	', 'quantidade' => '	678	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	75	', 'quantidade' => '	687	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	76	', 'quantidade' => '	696	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	77	', 'quantidade' => '	705	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	78	', 'quantidade' => '	714	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	79	', 'quantidade' => '	724	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	80	', 'quantidade' => '	733	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	81	', 'quantidade' => '	742	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	82	', 'quantidade' => '	751	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	83	', 'quantidade' => '	760	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	84	', 'quantidade' => '	769	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	85	', 'quantidade' => '	779	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	86	', 'quantidade' => '	788	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	87	', 'quantidade' => '	797	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	88	', 'quantidade' => '	806	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	89	', 'quantidade' => '	815	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	90	', 'quantidade' => '	824	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	91	', 'quantidade' => '	834	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	92	', 'quantidade' => '	843	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	93	', 'quantidade' => '	852	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	94	', 'quantidade' => '	861	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	95	', 'quantidade' => '	870	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	96	', 'quantidade' => '	879	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	97	', 'quantidade' => '	889	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	98	', 'quantidade' => '	898	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	99	', 'quantidade' => '	907	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	100	', 'quantidade' => '	916	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	101	', 'quantidade' => '	925	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	102	', 'quantidade' => '	934	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	103	', 'quantidade' => '	943	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	104	', 'quantidade' => '	953	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	105	', 'quantidade' => '	962	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	106	', 'quantidade' => '	971	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	107	', 'quantidade' => '	980	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	108	', 'quantidade' => '	989	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	109	', 'quantidade' => '	998	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	110	', 'quantidade' => '	1008	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	111	', 'quantidade' => '	1017	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	112	', 'quantidade' => '	1026	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	113	', 'quantidade' => '	1035	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	114	', 'quantidade' => '	1044	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	115	', 'quantidade' => '	1053	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	116	', 'quantidade' => '	1063	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	117	', 'quantidade' => '	1072	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	118	', 'quantidade' => '	1081	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	119	', 'quantidade' => '	1090	']);
+        DB::table('medidas_tanques')->insert(['equipamento_id' => '	7	 ', 'unidade_medida_id' => '	2	', 'medida' => '	120	', 'quantidade' => '	1099	']);
+    }
+}
