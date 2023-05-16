429464bf520319fef3fccef20b8d1c3e42589205
Jocinei da Rosa <jocinei.prog@gmail.com>
2023-05-16 19:16:09 -0300

    modificado view

---
+++ database/migrations/2023_05_14_184137_create_abastecimentos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAbastecimentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abastecimentos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('equipamento_id');
+            $table->foreign('equipamento_id')->references('id')->on('equipamentos');
+            $table->unsignedBigInteger('produto_id');
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            $table->double('quantidade', '10,2');
+            $table->date('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abastecimentos');
+    }
+}
--- resources/views/app/layouts copy/app.blade.php
+++
@@ -1,4 +0,0 @@
-@include('app.layouts.header')
-@include('app.layouts.left')
-@yield('content')
-@include('app.layouts.footer')
