f3102a19545a1b9059d94c61c608c8fc13a9e8a5
Jocinei da Rosa <jocinei.prog@gmail.com>
2023-06-13 07:52:42 -0300

    arrumando abastecimentos

--- app/Http/Controllers/AbastecimentoController.php
+++ app/Http/Controllers/AbastecimentoController.php
@@ -20,27 +20,28 @@ class AbastecimentoController extends Controller
      */
     public function index(Request $request)
     {
-        $filtro_equipamento='';
+        $filtro_equipamento = '';
         $equipamentos = Equipamento::all();
         if ($request->filtro_equipamento) {
-            $filtro_equipamento=$request->filtro_equipamento;
+            $filtro_equipamento = $request->filtro_equipamento;
             $abastecimentos = DB::table('abastecimentos as ab')
                 ->join('equipamentos as eq', 'eq.id', '=', 'ab.equipamento_id')
                 ->join('produtos as pd', 'pd.id', '=', 'ab.produto_id')
                 ->selectRaw('ab.id as id, eq.nome as equipamento, pd.nome as produto, ab.quantidade as quantidade, ab.data as data ')
-                ->where('eq.nome',  'like', '%' . $request->filtro_equipamento . '%')->orderBy('data','desc')->paginate(12);
+                ->where('eq.nome',  'like', '%' . $request->filtro_equipamento . '%')->orderBy('data', 'desc')->paginate(12);
         } else {
             $abastecimentos = DB::table('abastecimentos as ab')
                 ->join('equipamentos as eq', 'eq.id', '=', 'ab.equipamento_id')
                 ->join('produtos as pd', 'pd.id', '=', 'ab.produto_id')
                 ->selectRaw('ab.id as id, eq.nome as equipamento, pd.nome as produto, 
-                ab.quantidade as quantidade, ab.data as data ')->orderBy('data','desc')->paginate(12);
+                ab.quantidade as quantidade, ab.data as data ')->orderBy('data', 'desc')->paginate(12);
         }
 
 
-        return view('app.abastecimento.index', ['abastecimentos' => $abastecimentos,
-         'equipamentos' => $equipamentos, 'request' => $request->all(),
-        'filtro_equipamento'=>$filtro_equipamento
+        return view('app.abastecimento.index', [
+            'abastecimentos' => $abastecimentos,
+            'equipamentos' => $equipamentos, 'request' => $request->all(),
+            'filtro_equipamento' => $filtro_equipamento
         ]);
     }
 
@@ -70,19 +71,20 @@ class AbastecimentoController extends Controller
         $controle_saida = $controle->controle_saida;
 
         if ($controle_consumo == 1) {
-            $consumo= new Consumo();
-            $consumo->equipamento_id= $request->equipamento_id;
-            $consumo->produto_id= $request->produto_id;
-            $consumo->quantidade= $request->quantidade;
-            $consumo->data= $request->data;
+            $consumo = new Consumo();
+            $consumo->equipamento_id = $request->equipamento_id;
+            $consumo->produto_id = $request->produto_id;
+            $consumo->quantidade = $request->quantidade;
+            $consumo->data = $request->data;
             $consumo->save();
-            $request['consumo_id']=$consumo->id;
+            $request['consumo_id'] = $consumo->id;
         } else {
             $equipamento = Equipamento::find($request->equipamento_id);
             $equipamento->quant_tanque = $equipamento->quant_tanque + $request->quantidade;
             $equipamento->save();
         }
-        
+        ##################################################################
+        //caso o controle de saida for 0, a cada abastecimento gera uma saida
         if ($controle_saida == 0) {
             $saida_produto = new SaidaProduto();
             $saida_produto->equipamento_id = $request->equipamento_id;
@@ -90,7 +92,7 @@ class AbastecimentoController extends Controller
             $saida_produto->quantidade = $request->quantidade;
             $saida_produto->motivo = '1';
             $saida_produto->data = $request->data;
-            $saida_produto->consumo_id=$consumo->id;
+            $saida_produto->consumo_id = $consumo->id;
             $saida_produto->save();
 
             $produto = Produto::find($request->produto_id);
@@ -124,10 +126,9 @@ class AbastecimentoController extends Controller
      */
     public function edit(Abastecimento $abastecimento)
     {
-        $equipamentos=Equipamento::orderBy('nome', 'asc')->get();
-        $produtos=Produto::orderBy('nome', 'asc')->get();
-        return view('app.abastecimento.edit', ['abastecimento'=>$abastecimento, 'equipamentos'=>$equipamentos, 'produtos'=>$produtos]);
-
+        $equipamentos = Equipamento::orderBy('nome', 'asc')->get();
+        $produtos = Produto::orderBy('nome', 'asc')->get();
+        return view('app.abastecimento.edit', ['abastecimento' => $abastecimento, 'equipamentos' => $equipamentos, 'produtos' => $produtos]);
     }
 
     /**
@@ -144,23 +145,23 @@ class AbastecimentoController extends Controller
         $controle_saida = $controle->controle_saida;
 
         if ($controle_consumo == 1) {
-            $consumo=Consumo::find($abastecimento->consumo_id);
-            $consumo->equipamento_id= $request->equipamento_id;
-            $consumo->produto_id= $request->produto_id;
-            $consumo->quantidade= $request->quantidade;
-            $consumo->data= $request->data;
+            $consumo = Consumo::find($abastecimento->consumo_id);
+            $consumo->equipamento_id = $request->equipamento_id;
+            $consumo->produto_id = $request->produto_id;
+            $consumo->quantidade = $request->quantidade;
+            $consumo->data = $request->data;
             $consumo->save();
         } else {
             $equipamento = Equipamento::find($request->equipamento_id);
             $equipamento->quant_tanque = $equipamento->quant_tanque + $request->quantidade;
             $equipamento->save();
         }
-        
+
         if ($controle_saida == 0) {
-                        
+
             $saida_produto = SaidaProduto::where('consumo_id', $consumo->id);
 
-            $diferenca_atualizada=$request->quantidade - $saida_produto->quantidade;
+            $diferenca_atualizada = $request->quantidade - $saida_produto->quantidade;
             $produto = Produto::find($request->produto_id);
             $produto->estoque_atual = $produto->estoque_atual + $request->quantidade; // soma estoque antigo com a entrada de produto
             $produto->save();
@@ -171,7 +172,6 @@ class AbastecimentoController extends Controller
             $saida_produto->motivo = '1';
             $saida_produto->data = $request->data;
             $saida_produto->save();
-
         }
 
         $abastecimento->update($request->all());
@@ -185,13 +185,31 @@ class AbastecimentoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request)
     {
-        //
+        $abastecimento = Abastecimento::find($request->data_id);
+        $consumo = Consumo::find($abastecimento->consumo_id);
+        $equipamento = Equipamento::find($abastecimento->equipamento_id);
+        $controle_consumo = $equipamento->controle_consumo;
+        $controle_saida = $equipamento->controle_saida;
+
+        if(isset($controle_saida)){
+            echo'existe';
+            exit();
+            $saida_produto=SaidaProduto::where('consumo_id', $consumo->id)->first();
+            $saida_produto->delete();
+        }
+
+        $consumo->delete();
+
+
+        $abastecimento->delete();
+        return redirect()->route('abastecimento.index');
     }
 
-    public function pdfExport($equipamento=''){
-        if ($equipamento <>'') {
+    public function pdfExport($equipamento = '')
+    {
+        if ($equipamento <> '') {
             $abastecimentos = DB::table('abastecimentos as ab')
                 ->join('equipamentos as eq', 'eq.id', '=', 'ab.equipamento_id')
                 ->join('produtos as pd', 'pd.id', '=', 'ab.produto_id')
@@ -204,9 +222,9 @@ class AbastecimentoController extends Controller
                 ->selectRaw('ab.id as id, eq.nome as equipamento, pd.nome as produto, ab.quantidade as quantidade, ab.data as data ')->get();
         }
 
-        $total_quant=$abastecimentos->sum('quantidade');
+        $total_quant = $abastecimentos->sum('quantidade');
 
-        $pdf = PDF::loadView('app.abastecimento.exporta_pdf', ['abastecimentos' => $abastecimentos, 'total_quant'=>$total_quant]);
+        $pdf = PDF::loadView('app.abastecimento.exporta_pdf', ['abastecimentos' => $abastecimentos, 'total_quant' => $total_quant]);
         return $pdf->stream('Abastecimentos.pdf');
     }
 }
