390b7296f259d1c80f1ed473cd3a3b5697b0d483
Jocinei da Rosa <jocinei.prog@gmail.com>
2024-06-11 12:58:24 -0300

    criado modal informativo

--- public/js/innout.js
+++ public/js/innout.js
@@ -41,39 +41,44 @@ $(function () {
   var accordion = new Accordion($('.accordion-menu'), false);
 })
 
-/* 
+
 document.addEventListener("DOMContentLoaded", function () {
   var modal = document.getElementById("modalx");
   var closeButton = document.getElementById("close-button");
 
+  var modalShownValue = getCookie('modalShown');
+
+
+if (modalShownValue !== null && modalShownValue === 'true') {
+    return;
+}else {
+  modal.style.display='flex';
+}
+
   // Quando o usuário clicar no botão de fechar, o modal será fechado
   closeButton.onclick = function () {
+      setCookie('modalShown', 'true', 365);
       modal.style.display = "none";
   };
 
-  // Quando o usuário clicar em qualquer lugar fora do modal, o modal será fechado
-  window.onclick = function (event) {
-      if (event.target === modal) {
-          modal.style.display = "none";
-      }
-  };
-});
- */
 
-$(document).ready(function() {
-  // Verifica se o cookie 'modalShown' existe
-  if (!document.cookie.split(';').some((item) => item.trim().startsWith('modalShown='))) {
-      // Se não existe, mostra o modal
-      $('#modalx').show();
+});
+
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
   }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
 
-  // Quando o usuário clicar no botão de fechar, o modal será fechado
-  $('#close-button').on('click', function() {
-      $('#modalx').hide();
-      // Define o cookie 'modalShown' para evitar que o modal apareça novamente
-      document.cookie = "modalShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
-  });
-});
+function getCookie(name) {
+  var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+  if (match) return match[2];
+}
 
 
 
